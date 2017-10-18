9bfcb438011b19b33b445016cf7b234a6a52d6a1
Mitchell Kember <mk12360@gmail.com>
2017-10-17 21:23:41 -0400

    Make database connection work on Heroku

--- backend/watnotes/database.py
+++ backend/watnotes/database.py
@@ -4,10 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 from watnotes import app
 
+import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    "{dialect}+{driver}://{user}:{password}@{host}/{dbname}" \
-    .format(user=app.config['DB_USER'],
-            password=app.config['DB_PASSWORD'],
-            **app.config['DB_CONNECTION'])
+
+def get_database_uri(**params):
+    """Gets the database URI from the environment or configuration files."""
+    uri = os.environ.get('DATABASE_URL')
+    if uri:
+        return uri
+    uri = "{dialect}+{driver}://{user}:{password}@{host}/{dbname}"
+    return uri.format(user=app.config['DB_USER'],
+                      password=app.config['DB_PASSWORD'],
+                      **app.config['DB_CONNECTION'])
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = get_database_uri()
 db = SQLAlchemy(app)
