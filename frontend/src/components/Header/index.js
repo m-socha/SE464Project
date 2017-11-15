1bec490133ad08acf4a010d35fa1170db1e04ebb
Justin Kim <dg3kim@uwaterloo.ca>
2017-11-15 17:17:37 -0500

    Added a search box but not sure if look ahead is possible

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import {
   Navbar,
   NavItem,
+  Input,
+  Icon
 } from 'react-materialize';
 
 export default class Header extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      inquiry: ""
+    }
+  }
+
+  onChange(proxy, inquiry) {
+    this.setState({inquiry});
+  }
+
   render() {
     return (
       <Navbar className='row' brand='WatNotes' right>
+        <Input label="Search docs" value={this.state.inquiry} onChange={this.onChange.bind(this)}>
+          <Icon>search</Icon>
+        </Input>
         <NavItem href='/'>Home</NavItem>
         <NavItem href='/login'>Login</NavItem>
       </Navbar>
