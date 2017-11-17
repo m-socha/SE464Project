f52cdbcc3ec5a08e3845f707815e45c37f935ec6
Justin Kim <dg3kim@uwaterloo.ca>
2017-11-16 20:51:04 -0500

    search box ui done

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,31 +2,67 @@ import React from 'react';
 import {
   Navbar,
   NavItem,
-  Input,
-  Icon
+  Icon,
 } from 'react-materialize';
+import Autocomplete from 'react-autocomplete'
+import fuzzysearch from 'fuzzysearch'
+
 
 export default class Header extends React.Component {
   constructor() {
     super()
 
     this.state = {
-      inquiry: ""
+      inquiry: "",
+      data: [
+              {label: 'Apple'},
+              {label: 'Microsoft'},
+              {label: 'Google'}
+            ]
     }
   }
 
-  onChange(proxy, inquiry) {
+  onChange(inquiry) {
     this.setState({inquiry});
+  }
+
+  onSelect(inquiry) {
+    this.setState({inquiry});
+  }
+
+  renderItem(item, isHighlighted) {
+    return (
+      <div key={item.label} style={{color: 'black'}}>
+        {item.label}
+      </div>
+    );
+  }
+
+  shouldItemRender(item) {
+    let match = fuzzysearch(this.state.inquiry.toLowerCase(), item.label.toLowerCase());
+    return match;
   }
 
   render() {
     return (
       <Navbar className='row' brand='WatNotes' right>
-        <Input label="Search docs" value={this.state.inquiry} onChange={this.onChange.bind(this)}>
-          <Icon>search</Icon>
-        </Input>
         <NavItem href='/'>Home</NavItem>
         <NavItem href='/login'>Login</NavItem>
+        <li>
+          <Icon>search</Icon>
+        </li>
+        <li>
+          <Autocomplete
+            autoHighlight={false} 
+            getItemValue={(item) => item.label}
+            items={this.state.data}
+            shouldItemRender={this.shouldItemRender.bind(this)}
+            value={this.state.inquiry}
+            onChange={(e) => this.onChange(e.target.value)}
+            onSelect={(val) => this.onSelect(val)}
+            renderItem={this.renderItem}
+          />
+        </li>
       </Navbar>
     );
   }
