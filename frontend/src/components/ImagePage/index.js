6d05e7047813abcea8cbc240bbea5a68259cbe1a
Myungheon Chun <m5chun@edu.uwaterloo.ca>
2017-11-21 22:08:13 -0500

    Center image pages

--- frontend/src/components/ImagePage/index.js
+++ frontend/src/components/ImagePage/index.js
@@ -7,7 +7,9 @@ import config from 'react-global-configuration';
 const ImagePage = (props) => {
   return (
     <Page selectPageForComments={props.selectPageForComments} onComment={props.onComment} pageID={props.page.id}>
-      <img className={styles.imagepage} src={`${config.get('backend')}/notes/${props.page.id}.${props.format}`} alt="Single Note" />
+      <div className="center">
+        <img className={styles.imagepage} src={`${config.get('backend')}/notes/${props.page.id}.${props.format}`} alt="Single Note" />
+      </div>
     </Page>
   );
 };
